a711361c0a160bf1296fba8a17a44da97ef9c701
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-21 17:39:52 +0700

    membuat cetakan

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+class Mahasiswa{ //blue print atau cetakan
+public :
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData(){
+        cout << "Nim : "<< nim << endl;   
+        cout << "Nama : " << nama << endl;  
+        cout << "Nilai : " << nilai << endl;
+    }
+}; //batas class
